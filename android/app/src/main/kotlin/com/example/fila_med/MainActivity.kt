b62d4767189fc31be68b5857ce30a4fc7bd660fa
Marcoaudev <marcoau.dev@gmail.com>
2024-08-24 22:47:57 -0300

    starting project

---
+++ android/app/src/main/kotlin/com/example/fila_med/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fila_med
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:fila_med/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+
